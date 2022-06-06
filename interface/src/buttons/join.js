c55c27697f9638a81af3e3e832d3a2e6aa942f23
Sanford Pun <punsanford@gmail.com>
2022-06-06 14:40:00 +0800

    Handle `join` button

--- interface/src/buttons/join.js
+++ interface/src/buttons/join.js
@@ -1,6 +1,29 @@
+const { createPrepareMsgOptions } = require("../commands/prepare");
+
 module.exports = {
     name: "join",
     async handle(interaction) {
+        // Format: join_<ownerId>
+        const buttonId = interaction.customId;
+        const ownerId = buttonId.split("_")[1];
 
+        const user = interaction.user;
+        const userMention = user.toString();
+
+        const builder = interaction.client.gameInstanceManager.getBuilder(ownerId);
+        const metadata = interaction.client.gameInstanceManager.getBuilderMetadata(ownerId);
+
+        if (metadata.playerMentions.includes(userMention)) {
+            await interaction.reply({
+                content: "You have already joined this game!",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        builder.addPlayer(userMention);
+
+        metadata.playerMentions.push(userMention);
+        await metadata.prepareMsg.edit(createPrepareMsgOptions(ownerId, metadata.playerMentions));
     },
 };
