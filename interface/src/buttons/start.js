25690bf9cd677670cc1e25608e79bbed02e95547
Sanford Pun <punsanford@gmail.com>
2022-06-06 23:54:19 +0800

    Move startRound() to buttons/start.js

--- interface/src/buttons/start.js
+++ interface/src/buttons/start.js
@@ -1,4 +1,51 @@
-const { startRound } = require("../util");
+const { MessageActionRow, MessageButton } = require("discord.js");
+
+function formatPlayers(players, judge) {
+    let content = "Take turns to be the **Judge**!\n";
+
+    for (const player of players) {
+        content += judge === player ? `🧑‍⚖️ ${player} 👩‍⚖️` : player;
+        content += "\n";
+    }
+
+    return content;
+}
+
+function formatQuestion(question) {
+    let content = "===================================\n";
+
+    content += "Question:\n\n";
+
+    content += `**${question.replaceAll("_", "❓")}**`;
+
+    const components = [
+        new MessageActionRow().addComponents(
+            new MessageButton()
+                .setCustomId("hand")
+                .setLabel("View Hand")
+                .setStyle("PRIMARY"),
+        ),
+    ];
+
+    return { content, components };
+}
+
+async function startRound(driver, channel, metadata) {
+    const players = driver.orderedPlayers();
+    const { judge, question, playerHands } = driver.startRound();
+
+    await channel.send(formatPlayers(players, judge));
+
+    await channel.send(formatQuestion(question));
+
+    metadata.judge = judge;
+    metadata.playerHands = playerHands;
+    metadata.playerSelections = {};
+    metadata.playerHandInteractions = {};
+    metadata.playerAnswerInteractions = {};
+    metadata.playerSubmitInteractions = {};
+    metadata.submitResult = [];
+}
 
 module.exports = {
     name: "start",
@@ -43,7 +90,8 @@ module.exports = {
             components: [],
         });
         interaction.client.gameInstanceManager.insertDriver(thread.id, driver);
-        const driverMetadata = interaction.client.gameInstanceManager.getDriverMetadata(thread.id);
+        const driverMetadata =
+            interaction.client.gameInstanceManager.getDriverMetadata(thread.id);
 
         await startRound(driver, thread, driverMetadata);
     },
