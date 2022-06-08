f15997feaea79b7cab2ca6b1f07ce49d79841ebe
Sanford Pun <punsanford@gmail.com>
2022-06-08 11:10:49 +0800

    Small refactoring

--- interface/src/asset_loader.js
+++ interface/src/asset_loader.js
@@ -1,17 +1,17 @@
 const path = require("node:path");
 
 class AssetLoader {
-    static LoadQuestionsJson(filename) {
-        const loadPath = path.join(AssetLoader.BasePath, "questions", filename + ".json");
+    static LoadQuestionsJson(libraryName) {
+        const loadPath = path.join(AssetLoader.LocalBasePath, "questions", libraryName + ".json");
         return require(loadPath);
     }
 
-    static LoadAnswersJson(filename) {
-        const loadPath = path.join(AssetLoader.BasePath, "answers", filename + ".json");
+    static LoadAnswersJson(libraryName) {
+        const loadPath = path.join(AssetLoader.LocalBasePath, "answers", libraryName + ".json");
         return require(loadPath);
     }
 }
 
-AssetLoader.BasePath = path.join(__dirname, "..", "assets");
+AssetLoader.LocalBasePath = path.join(__dirname, "..", "assets");
 
 module.exports = AssetLoader;
