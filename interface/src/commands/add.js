7b0dff63010a04160506e9a7c32d645134c07867
Sanford Pun <punsanford@gmail.com>
2022-06-08 14:46:55 +0800

    Handle `add` command

---
+++ interface/src/commands/add.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const AssetLoader = require("../asset_loader");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("add")
+        .setDescription("Add questions or answers!")
+        .addStringOption((option) =>
+            option
+                .setName("type")
+                .setDescription("What to add?")
+                .setRequired(true)
+                .addChoices(
+                    { name: "questions", value: "questions" },
+                    { name: "answers", value: "answers" },
+                ),
+        )
+        .addStringOption((option) =>
+            option
+                .setName("optional_library")
+                .setDescription("Name of the library/worksheet suffix"),
+        ),
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        const userId = interaction.user.id;
+        const builder = interaction.client.gameInstanceManager.getBuilder(userId);
+
+        const libraryName = interaction.options.getString("library") ?? "default";
+
+        let reply = "Adding";
+        let data = [];
+        switch (interaction.options.getString("type")) {
+            case "questions":
+                data = await AssetLoader.LoadQuestionsJson(libraryName);
+                builder.addNewQuestions(data);
+                reply = `Successfully added ${data.length} questions in ${libraryName}.`;
+                break;
+            case "answers":
+                data = await AssetLoader.LoadAnswersJson(libraryName);
+                builder.addNewAnswers(data);
+                reply = `Successfully added ${data.length} questions in ${libraryName}.`;
+                break;
+            default:
+                throw "Unreachable: Unknown fetch `type`.";
+        }
+
+        await interaction.editReply(reply);
+    },
+};
